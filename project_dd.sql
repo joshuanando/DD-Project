c9b000aa00d1308298a485465a06aa7f08e8951a
Bryant Pirih <74889629+BryantPirih@users.noreply.github.com>
2022-12-22 18:57:25 +0700

    history stok sql

--- project_dd.sql
+++ project_dd.sql
@@ -80,6 +80,15 @@ CREATE TABLE DTRANS (
 	FOREIGN KEY (ID_HTRANS) REFERENCES HTRANS(ID_Transaksi)
 );
 
+create table history_stok(
+    id varchar2(10),
+    nama_barang varchar2(20),
+    stok_sebelum number(3),
+    stok_sesudah number(3),
+    keterangan varchar2(30),
+    tanggal date
+);
+
 --------------------TRIGGERS--------------------------
 --ID TOOLS
 CREATE OR REPLACE TRIGGER create_id_tools 
@@ -302,6 +311,46 @@ END autoSumHtrans;
 /
 show err;
 COMMIT;
+
+--id history stok
+Create or replace Trigger autoIdHistoryStok
+before insert 
+    on history_stok
+    for each row
+declare 
+    temp_id varchar2(10);
+	localid VARCHAR2(10);
+    err exception;
+begin
+	select max(id) into temp_id from history_stok;
+	select '&LOCALID' into localid from dual;	
+	
+	if temp_id IS NULL then
+		temp_id:=1;
+	ELSE 
+	temp_id := substr(temp_id,-3,3)+1;
+	end if;
+	:new.id := localid || '/H' ||lpad(temp_id,3,'0');
+exception 
+    when err then raise_application_error(-20008,'hangus');
+END;
+/
+show err;
+
+--trigger update history
+Create or replace Trigger updateStokHistory
+after update 
+    on sparepart
+    for each row
+declare 
+    err exception;
+begin
+  insert into history_stok values('',:new.name,:old.stok ,:new.stok,'restok barang', current_date);
+exception 
+    when err then raise_application_error(-20009,'hangus');
+END;
+/
+show err;
 
 
 ----------------------------------------------
