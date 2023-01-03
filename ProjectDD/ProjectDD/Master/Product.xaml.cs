b6bc4fca6d5154ecf8980063a6ff3f5014a272bc
joshuanando <joshuanando7@gmail.com>
2023-01-03 11:03:50 +0700

    Sparepart (update)

--- ProjectDD/ProjectDD/Master/Product.xaml.cs
+++ ProjectDD/ProjectDD/Master/Product.xaml.cs
@@ -5,6 +5,8 @@ using System.Data.OracleClient;
 using WPFCustomMessageBox;
 using System.Windows;
 using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
 
 namespace ProjectDD.Master
 {
@@ -15,6 +17,7 @@ namespace ProjectDD.Master
     {
         DataTable dt;
         OracleConnection conn;
+        List<Tools_Category> listkat = new List<Tools_Category>();
 
         List<db_cab> listcabang = new List<db_cab>()
         {
@@ -24,11 +27,56 @@ namespace ProjectDD.Master
             new db_cab() { nama_cabang = "jon", nama_db = "sparepart_cabjon"},
         };
 
+        public void hideUpdate(bool flag){
+            if (flag)
+            {
+                gbUpSpare.Visibility = Visibility.Hidden;
+                lblUpID.Visibility = Visibility.Hidden;
+                txtUpIdSpare.Visibility = Visibility.Hidden;
+                lblUpNama.Visibility = Visibility.Hidden;
+                txtUpNama.Visibility = Visibility.Hidden;
+                lblUpCat.Visibility = Visibility.Hidden;
+                cbCategorySpare.Visibility = Visibility.Hidden;
+                lblUpStok.Visibility = Visibility.Hidden;
+                txtUpStok.Visibility = Visibility.Hidden;
+                lblUpHarga.Visibility = Visibility.Hidden;
+                txtUpHarga.Visibility = Visibility.Hidden;
+                btnUpSparepart.Visibility = Visibility.Hidden;
+                lblUpDesc.Visibility = Visibility.Hidden;
+                rtbUpDesc.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                loadKategori();
+                gbUpSpare.Visibility = Visibility.Visible;
+                lblUpID.Visibility = Visibility.Visible;
+                txtUpIdSpare.Visibility = Visibility.Visible;
+                lblUpNama.Visibility = Visibility.Visible;
+                txtUpNama.Visibility = Visibility.Visible;
+                lblUpCat.Visibility = Visibility.Visible;
+                cbCategorySpare.Visibility = Visibility.Visible;
+                lblUpStok.Visibility = Visibility.Visible;
+                txtUpStok.Visibility = Visibility.Visible;
+                lblUpHarga.Visibility = Visibility.Visible;
+                txtUpHarga.Visibility = Visibility.Visible;
+                btnUpSparepart.Visibility = Visibility.Visible;
+                lblUpDesc.Visibility = Visibility.Visible;
+                rtbUpDesc.Visibility = Visibility.Visible;
+            }
+        }
+
         public Product(OracleConnection c)
         {
             InitializeComponent();
             c = conn;
             init();
+            hideUpdate(true);
+        }
+
+        private static readonly Regex _regex = new Regex("^[^0-9]"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
         }
 
         public void init()
@@ -66,6 +114,35 @@ namespace ProjectDD.Master
             dgProduct.ItemsSource = dt.DefaultView;
             connection.closeConn();
         }
+        private void loadKategori()
+        {
+            connection.openConn();
+            try
+            {
+                listkat = new List<Tools_Category>();
+                cbCategorySpare.Items.Clear();
+                //OracleCommand cmd = new OracleCommand("select ID, NAMA from tools_category", conn.cabangnow);
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = connection.conn;
+                cmd.CommandText = "select ID_CATEGORY, CATEGORY_NAME from admin.sparepart_category";
+                //conn.Open();
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listkat.Add(new Tools_Category(reader.GetValue(0).ToString(), reader.GetValue(1).ToString()));
+                }
+                reader.Close();
+                cbCategorySpare.ItemsSource = listkat;
+                cbCategorySpare.DisplayMemberPath = "nama";
+                cbCategorySpare.SelectedValuePath = "id";
+                cbCategorySpare.SelectedIndex = 0;
+                //conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "");
+            }
+        }
 
         private void btnTampil_Click_1(object sender, RoutedEventArgs e)
         {
@@ -82,6 +159,7 @@ namespace ProjectDD.Master
 
         private void btnView_Click(object sender, RoutedEventArgs e)
         {
+            int idx = dgProduct.SelectedIndex + 1;
             try
             {
                 DataRowView dataRowView = (DataRowView)((Button)e.Source).DataContext;
@@ -94,10 +172,16 @@ namespace ProjectDD.Master
                     "Update / Delete Sparepart",
                     "Update!",
                     "Cancel!") == MessageBoxResult.OK){
-                    //doupdate
-                    //coding
-                }else{
-                    
+                    //doUpdate
+                    hideUpdate(false);
+                    txtUpIdSpare.Text = dataRowView[0].ToString();
+                    txtUpNama.Text = dataRowView[1].ToString();
+                    txtUpStok.Text = dataRowView[3].ToString();
+                    txtUpHarga.Text = dataRowView[4].ToString();
+                    rtbUpDesc.Document.Blocks.Add(new Paragraph(new Run(dataRowView[5].ToString())));
+                }
+                else{
+                    //doDelete
                 }
             }
             catch (Exception ex)
@@ -113,8 +197,6 @@ namespace ProjectDD.Master
             ins_sparepart.Show();
         }
 
-        
-
         private void btnRefreshSparepart1_Clicked(object sender, RoutedEventArgs e)
         {
             connection.openConn();
@@ -125,6 +207,50 @@ namespace ProjectDD.Master
             //MessageBox.Show(cmd.CommandText);
             cmd.ExecuteNonQuery();
             connection.closeConn();
+        }
+
+        private void txtUpStok_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e) => e.Handled = !IsTextAllowed(e.Text);
+
+        private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e) => e.Handled = !IsTextAllowed(e.Text);
+
+        private void btnUpSparepart_Click(object sender, RoutedEventArgs e)
+        {
+            string tempCate = cbCategorySpare.SelectedItem.ToString();
+            string tempId = "";
+            for (int i = 0; i < listkat.Count; i++)
+            {
+                if (tempCate.Equals(listkat[i].ToString()))
+                {
+                    tempId = listkat[i].id.ToString();
+                }
+            }
+            string richDesc = new TextRange(rtbUpDesc.Document.ContentStart, rtbUpDesc.Document.ContentEnd).Text;
+
+            using (OracleTransaction trans = connection.conn.BeginTransaction())
+            {
+                try
+                {
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = connection.conn;
+                    cmd.CommandText = "UPDATE ADMIN.SPAREPART SET NAME=:name, ID_CATEGORY=:id_category, STOK=:stok, HARGA=:harga, DESCRIPTION=:desc where ID_SPARE=:id_spare";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(":name", txtUpNama.Text);
+                    cmd.Parameters.Add(":id_category", tempId);
+                    cmd.Parameters.Add(":stok", txtUpStok);
+                    cmd.Parameters.Add(":harga", txtUpHarga);
+                    cmd.Parameters.Add(":desc", richDesc);
+                    cmd.Parameters.Add(":id_spare", txtUpIdSpare);
+                    cmd.Transaction = trans;
+                    cmd.ExecuteNonQuery();
+                    trans.Commit();
+                    MessageBox.Show("Berhasil Update!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    trans.Rollback();
+                }
+            }
         }
     }
 }
